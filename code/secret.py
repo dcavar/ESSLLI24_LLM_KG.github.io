d79ae569d213a18be3ac300644d67b4236d167fa
Damir Cavar <dcavar@me.com>
2024-08-01 07:18:17 +0200

    added more code examples

--- code/secret.py
+++ code/secret.py
@@ -10,3 +10,15 @@ claude3_apikey = ""
 # OpenAI API key
 apikey = ""
 
+
+# AllegroGraph credentials
+ag_host = "127.0.0.1"
+ag_port = "10035"
+ag_user = "username"
+ag_password = "password"
+
+# Neo4j credentials
+neo4j_login = ""
+neo4j_password = ""
+neo4j_uri = ""
+
